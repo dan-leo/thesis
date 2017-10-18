2a02ad90ca4063aaccde13ea932eb364363012a5
Daniel Robinson <d7robin@gmail.com>
2017-10-19 00:50:33 +0200

    Added to chapter 3

--- .python/dual_list_maker.py
+++ .python/dual_list_maker.py
@@ -2,9 +2,10 @@ import glob
 
 root_dir = 'C:/Users/d7rob/thesis/distorted/undistorted'
 root_dir = 'L:/Backups/thesis/longridge'
+root_dir = 'C:/Users/d7rob/thesis/home_lab_window_2'
 
-_rgb_name = "/rgb_undistorted"
-_ir_name = "/ir_undistorted"
+_rgb_name = "/rgb_blue"
+_ir_name = "/ir_blue"
 rgb_images = glob.glob(root_dir + _rgb_name + '/*.jpg')
 ir_images = glob.glob(root_dir + _ir_name + '/*.jpg')
 
